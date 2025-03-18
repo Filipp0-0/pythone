52efee4586516633958063ca892a6203c4a7a1ac
Filipp0-0 <filippomartino1607@gmail.com>
2025-03-18 13:01:01 +0100

    Create somma.py

---
+++ somma.py
@@ -0,0 +1,9 @@
+#somma.py
+
+print("Facciamo la somma di due numeri")
+valore_1 = input("Passami il primo valore: ")
+valore_2 = input("Passami il secondo valore: ")
+v1 =int(valore_1)
+v2= int(valore_2)
+totale_somma = v1 + v2
+print("la somma dei due numeri è", totale_somma)
