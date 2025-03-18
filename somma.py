83c37e9c3a9702039880a4fb68df2c0fd4c17374
Filipp0-0 <filippomartino1607@gmail.com>
2025-03-18 13:10:00 +0100

    Update somma.py

--- somma.py
+++ somma.py
@@ -5,5 +5,5 @@ valore_1 = input("Passami il primo valore: ")
 valore_2 = input("Passami il secondo valore: ")
 v1 =int(valore_1)
 v2= int(valore_2)
-totale_somma = v1 + v2
-print("La somma dei due numeri è", totale_somma)
+somma = v1 + v2
+print("La somma dei due numeri è", somma)
