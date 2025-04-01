d8a32fa8aaf1831b9596d2accf5437678b97e1a1
Filipp0-0 <filippomartino1607@gmail.com>
2025-04-01 10:17:30 +0200

    Update carte_da_poker.py

--- carte_da_poker.py
+++ carte_da_poker.py
@@ -1,5 +1,5 @@
-#carte_da_pocker.py
-#Identificare  i caratteri unicode che rappresentano i  4 semi del carte da pocker (cuori, quadri, fiori e picche) nella variante solo bordo. Creare una stringa con i 4 semi.
+#carte_da_poker.py
+#Identificare  i caratteri unicode che rappresentano i  4 semi del carte da poker (cuori, quadri, fiori e picche) nella variante solo bordo. Creare una stringa con i 4 semi.
 
 solo_byte = b'\xe2\x99\xa1\xe2\x99\xa2\xe2\x99\xa7\xe2\x99\xa4'  
 stringa = solo_byte.decode()
