97d9ea7fb8eb9e69b89dbce98e6153a4a79cc674
Filipp0-0 <filippomartino1607@gmail.com>
2025-04-01 16:26:46 +0200

    Update media.py

--- media.py
+++ media.py
@@ -1,3 +1,4 @@
+
 #media.py
 #Acquisire tramite input una sequenza di valori numerici fino a che l'utewnte non scriva "exit" 
 #In tal caso stampare la media dei valori inseriti 
@@ -11,16 +12,20 @@ while numero != "exit":
 
     try:
         numero = float(numero)
-        somma += numero
-        contatore += 1
 
-    #per gestire l'errore se l'utente inserisce un valore non numerico
     except ValueError:  
-        print("Valore non valido, inserisci un numero o 'exit' per terminare.") 
-
-
+      print("Valore non valido, inserisci un numero o 'exit' per terminare.") 
     numero = input("Inserisci un numero, digita 'exit' per terminare: ")
+
+    continue   
+somma += numero
+contatore += 1
+numero = input("Inserisci un numero, digita 'exit' per terminare: ")
 
 if contatore > 0:
     media = somma / contatore
+    print("la somma dei valori inseriti è:", somma)
     print("La media dei valori inseriti è:", media)
+
+
+
