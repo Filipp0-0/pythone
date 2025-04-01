119e78ed363c520ac507f4e70966bedab3394d86
Filipp0-0 <filippomartino1607@gmail.com>
2025-04-01 10:13:50 +0200

    Update carte_da_pocker.py

--- carte_da_pocker.py
+++ carte_da_pocker.py
@@ -2,6 +2,6 @@
 #Identificare  i caratteri unicode che rappresentano i  4 semi del carte da pocker (cuori, quadri, fiori e picche) nella variante solo bordo. Creare una stringa con i 4 semi.
 
 solo_byte = b'\xe2\x99\xa1\xe2\x99\xa2\xe2\x99\xa7\xe2\x99\xa4'  
-stringa = solo_byte.decode('utf-8')
+stringa = solo_byte.decode()
 print(stringa)
 
